cb58820c42268439e275c5346f5da50c7ca19706
moussa ndong <moussabaye94@egmail.com>
2022-02-01 18:42:22 +0100

    filename comment debug

--- contrat_pdf.py
+++ contrat_pdf.py
@@ -327,7 +327,7 @@ def contrat_pdf():
     footer(pdf) 
     pdf.set_auto_page_break(True)   
   response = make_response(pdf.output(dest='S'))
-  #response.headers.set('Content-Disposition', 'attachment', filename=filename + '.pdf')
+  response.headers.set('Content-Disposition', 'attachment', filename=filename + '.pdf')
   response.headers.set('Content-Type', 'application/pdf')
   return response
 
